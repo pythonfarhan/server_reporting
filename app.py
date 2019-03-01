50f374dd38312d1bf2c289697eec32da840d4859
farhan <farrrrrhan@gmail.com>
2019-03-01 08:48:38 +0700

    fixing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def send_report(note="ERROR", exception_content=None):
     method = 'sendMessage'
     url = 'https://api.telegram.org/bot%s/%s' % (_constants.TOKEN, method)
 
-    text = "%s: \n%s\ntime: %s" % (note, str(exception_content), datetime.datetime.now())
+    text = "%s: \n%s\nat: %s" % (note, str(exception_content), datetime.datetime.now())
 
     data = {
         'chat_id': _constants.CHAT_ID,
